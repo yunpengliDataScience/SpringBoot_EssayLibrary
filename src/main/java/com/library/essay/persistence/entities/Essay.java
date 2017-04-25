21b53fc49221da35fcde8e44c937fe727c748d05
yunpeng.li <yunpeng.li@baesystems.com>
2017-04-25 14:04:29 -0400

    Use JavaDB (Derby)

--- src/main/java/com/library/essay/persistence/entities/Essay.java
+++ src/main/java/com/library/essay/persistence/entities/Essay.java
@@ -18,61 +18,62 @@ import org.hibernate.search.annotations.Indexed;
 @Indexed
 public class Essay implements Serializable {
 
-	@Id
-	@Column(name = "ESSAY_ID")
-	@GeneratedValue
-	@DocumentId
-	private Long id;
+  @Id
+  @Column(name = "ESSAY_ID")
+  @GeneratedValue
+  @DocumentId
+  private Long id;
 
-	@Column(name = "TITLE")
-	@Field
-	private String title;
+  @Column(name = "TITLE")
+  @Field
+  private String title;
 
-	@Column(name = "AUTHOR")
-	@Field
-	private String author;
+  @Column(name = "AUTHOR")
+  @Field
+  private String author;
 
-	@Lob
-	@Column(name = "CONTENT")
-	@Field
-	private String content;
+  @Lob
+  // Solve issue for Derby: Derby “A truncation error was encountered trying to shrink CLOB
+  // '<stream-value>' to length 255”
+  @Column(name = "CONTENT", columnDefinition = "clob")
+  @Field
+  private String content;
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public String getTitle() {
-		return title;
-	}
+  public String getTitle() {
+    return title;
+  }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-	public String getAuthor() {
-		return author;
-	}
+  public String getAuthor() {
+    return author;
+  }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
-	public String getContent() {
-		return content;
-	}
+  public String getContent() {
+    return content;
+  }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+  public void setContent(String content) {
+    this.content = content;
+  }
 
-	@Override
-	public String toString() {
-		return "Id: " + id + ", Title: " + title + ", Author: " + author
-				+ ", Content: " + content;
+  @Override
+  public String toString() {
+    return "Id: " + id + ", Title: " + title + ", Author: " + author + ", Content: " + content;
 
-	}
+  }
 }
--- src/main/java/com/library/essay/persistence/entities/User.java
+++ src/main/java/com/library/essay/persistence/entities/User.java
@@ -14,7 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "USER")
+@Table(name = "ESSAY_USER")
 public class User implements Serializable {
 
 	@Id
